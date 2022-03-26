6db4ec739b09247f1831ce6316c1743a6cfb1012
Caroline <caro672g@stud.kea.dk>
2022-03-26 12:32:30 +0100

    1st attempt js

--- main.js
+++ main.js
@@ -37,6 +37,15 @@ function showPosts(bikeData) {
 
     //change it
     copy.querySelector(".brand h2").textContent = singleBike.title.rendered;
+    copy.querySelector(".productCard img").src =
+      singleBike._embedded[
+        "wp:featuredmedia"
+      ][0].media_details.sizes.medium.source_url;
+    copy.querySelector(".brand h3").textContent =
+      singleBike._embedded["wp:term"][0][0].name;
+    copy.querySelector(".price").textContent = "$" + singleBike.price;
+    copy.querySelector(".colour").textContent = singleBike.colour;
+    copy.querySelector(".stock").textContent = singleBike.number_in_stock;
     //grab parent
     const parent = document.querySelector(".productList");
 
